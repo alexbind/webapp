eecf50c443df4de30e4ebabad4cc0f65d550fb2c
Alexander <alexandersilva436@gmail.com>
2021-08-21 10:35:17 -0300

    adição de novos arquivos

--- App.tsx
+++ App.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
-import { SignIn } from './src/screens/SignIn';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { SourceSansPro_700Bold, SourceSansPro_400Regular } from '@expo-google-fonts/source-sans-pro';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SignIn } from './src/screens/SignIn'
+import { Home } from './src/screens/Home'
 
-export default function App(){
-  return(
-   <SignIn />
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator
+    screenOptions={{
+      headerShown: false
+    }}
+    >
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
   );
+}
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    SourceSansPro_700Bold,
+    SourceSansPro_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  )
 }
